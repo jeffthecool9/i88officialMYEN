66c261f9f54df9e859e42f0c1ee2c4a521ccdbd0
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-06-12 00:52:20 +0800

    Update StickyCTA.jsx

--- src/components/StickyCTA.jsx
+++ src/components/StickyCTA.jsx
@@ -25,7 +25,7 @@ export default function StickyCTA() {
         {/* CTA */}
         <PremiumButton
           size="sm"
-          onClick={() => { trackEvent('sticky_cta_click', { section: 'sticky_bar' }); window.open('https://www.palacehub8.com/iszt3scu') }}
+          onClick={() => { trackEvent('sticky_cta_click', { section: 'sticky_bar' }); window.open('https://www.palacehub8.com/fEtYDN75') }}
           wrapperClassName="flex-1 sm:flex-none"
           className="w-full"
         >
